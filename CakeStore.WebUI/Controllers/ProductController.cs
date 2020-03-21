8a6cde7be467e7d3cfe1f57f006002798d57e346
Paweł Dziwura <paweldziwura1881997@gmail.com>
2020-03-21 18:08:45 +0100

    Add to Git

---
+++ CakeStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CakeStore.Domain.Abstract;
+using CakeStore.Domain.Entities;
+
+namespace CakeStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
