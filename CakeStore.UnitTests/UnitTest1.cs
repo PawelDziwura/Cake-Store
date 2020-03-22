40f4203ce84238df925dbb46dc391cb6532c3076
Paweł Dziwura <paweldziwura1881997@gmail.com>
2020-03-22 15:13:45 +0100

    Dodanie Kategorii

--- CakeStore.UnitTests/UnitTest1.cs
+++ CakeStore.UnitTests/UnitTest1.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using CakeStore.Domain.Abstract;
+using CakeStore.Domain.Entities;
+using CakeStore.WebUI.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CakeStore.UnitTests
@@ -7,8 +13,24 @@ namespace CakeStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            //przygotowanie
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            });
+            ProductController productController = new ProductController(mock.Object);
+            productController.PageSize = 3;
+
+            //działanie
+            //IEnumerable<Product> result =
+            //    (IEnumerable<Product>)productController.List(2).Model;
         }
     }
 }
